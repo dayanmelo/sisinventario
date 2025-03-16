ec3c99b6643850e64da52166b7ea12d00167f7ca
dayanmelo <157970716+dayanmelo@users.noreply.github.com>
2025-03-15 19:49:57 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -44,10 +44,21 @@ return [
         ],
 
         'pgsql' => [
+    'driver' => 'pgsql',
+    'host' => env('DB_HOST', 'localhost'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'schema' => 'public',
+    'sslmode' => 'prefer',
+
     'dump' => [
-        'dump_binary_path' => '/usr/bin/', // Ruta donde está pg_dump
+        'dump_binary_path' => '/usr/bin/', // Asegúrate de que sea correcto
         'use_single_transaction' => true,
-        'timeout' => 60, // Tiempo límite para el dump
+        'timeout' => 60, 
     ],
 ],
 
